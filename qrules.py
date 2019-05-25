da0a65d1d4658711c0670c0c5f4dfd1c8db09884
danielbultrini <danielbultrini@gmail.com>
2019-05-25 15:38:01 +0200

    Update qrules.py

--- qrules.py
+++ qrules.py
@@ -28,7 +28,8 @@ def SQGOL(nhood):
         value = (np.sqrt(2)+1)*(4-a)*alive+(a-3)*dead
     else:
         value = dead
-    value = value/np.linalg.norm(value)
+    value = value/np.sqrt(2)
+    print(value)
     return value 
 
 def DSQGOL(nhood):
@@ -36,16 +37,43 @@ def DSQGOL(nhood):
     value =  nhood[1,1]
     alive = [0,1]
     dead = [1,0]
-    if (a < 1 ):
-        value = dead
-    elif (a > 1 and a <= 1.5):
-        value = dead
-    elif (a > 1.5 and a <= 2.5):
-        value = 0
-    elif (a > 2.5 and a <= 3.5):
-        value = 0
-    elif (a > 3.5 and a <= 4.5):
-        value = 0
-    elif (a > 4.5):
-        value = dead
+    if value[1] > 0.98:
+        if (a < 1 ):
+            value = dead
+        elif (a > 1 and a <= 1.5):
+            value = dead
+        elif (a > 1.5 and a <= 2.5):
+            value = 0
+        elif (a > 2.5 and a <= 3.5):
+            value = 0
+        elif (a > 3.5 and a <= 4.5):
+            value = 0
+        elif (a > 4.5):
+            value = dead
+    if value[1] < 0.02:
+        if (a < 1 ):
+            value = dead
+        elif (a > 1 and a <= 1.5):
+            value = dead
+        elif (a > 1.5 and a <= 2.5):
+            value = 0
+        elif (a > 2.5 and a <= 3.5):
+            value = 0
+        elif (a > 3.5 and a <= 4.5):
+            value = 0
+        elif (a > 4.5):
+            value = dead   
+    else:
+        if (a < 1 ):
+            value = dead
+        elif (a > 1 and a <= 1.5):
+            value = dead
+        elif (a > 1.5 and a <= 2.5):
+            value = 0
+        elif (a > 2.5 and a <= 3.5):
+            value = 0
+        elif (a > 3.5 and a <= 4.5):
+            value = 0
+        elif (a > 4.5):
+            value = dead     
     
