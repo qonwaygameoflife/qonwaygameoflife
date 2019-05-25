bbae529d6d8f4a249114a14ef8c94d449068f1a7
danielbultrini <danielbultrini@gmail.com>
2019-05-25 16:55:38 +0200

    Update qrules.py

--- qrules.py
+++ qrules.py
@@ -23,11 +23,11 @@ def SQGOL(nhood):
     if a <= 1:
         value =  dead
     elif (a > 1 and a <= 2):
-        value = ((2-a)*dead+(a-1)*value)#(((np.sqrt(2)+1)*(2-a))**2+(a-1)**2)
+        value = ((np.sqrt(2)+1)*(2-a)*dead+(a-1)*value)#(((np.sqrt(2)+1)*(2-a))**2+(a-1)**2)
     elif (a > 2 and a <= 3):
-        value = ((3-a)*value+(a-2)*alive)#(((np.sqrt(2)+1)*(3-a))**2+(a-2)**2)
+        value = ((np.sqrt(2)+1)*(3-a)*value+(a-2)*alive)#(((np.sqrt(2)+1)*(3-a))**2+(a-2)**2)
     elif (a > 3 and a <= 4):
-        value = ((4-a)*dead+(a-3)*alive)#(((np.sqrt(2)+1)*(4-a))**2+(a-3)**2)
+        value = ((np.sqrt(2)+1)*(4-a)*dead+(a-3)*alive)#(((np.sqrt(2)+1)*(4-a))**2+(a-3)**2)
     elif a > 4:
         value = dead
     
