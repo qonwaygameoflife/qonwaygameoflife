6010b93c2a06b6d2c07c76ffe1909b72fcbf30dc
XWu <xiangnan.wu@estudiante.uam.es>
2019-05-26 09:57:12 +0200

    F quantum

--- qrules.py
+++ qrules.py
@@ -34,77 +34,47 @@ def SQGOL(nhood):
 
 def init_quantum(nhood):
     v=nhood
-<<<<<<< HEAD
     a = (v[0][0]+v[0][1]+v[0][2]+v[1][0]+v[1][2]+v[2][0]+v[2][1]+v[2][2])/8
     a = a/np.linalg.norm(a)
-=======
-    av = (v[0][0]+v[0][1]+v[0][2]+v[1][0]+v[1][2]+v[2][0]+v[2][1]+v[2][2])/8
-    print(av)
-    av = av/np.linalg.norm(av)
->>>>>>> 53f6f358e693a66712f6c984395e7787c7fa4546
     qr = QuantumRegister(3,'qr')
     qc = QuantumCircuit(qr,name='conway')
     counter  = 0
     initial_state = (1/np.sqrt(6))*np.array([2,1,0,1])
     qc.initialize(initial_state,[qr[1],qr[2]])
-<<<<<<< HEAD
     qc.initialize(a,[qr[0]])
     qc.cx(qr[0],qr[1])
-=======
     qc.initialize(av,[qr[0]])
-    qc.cx(qr[0],qr[1]) 
->>>>>>> 53f6f358e693a66712f6c984395e7787c7fa4546
+    qc.cx(qr[0],qr[1])
     qc.cx(qr[0],qr[2])
     qc.cx(qr[1],qr[0])
     qc.cx(qr[2],qr[0])
     job = execute(qc,Aer.get_backend('statevector_simulator'))
     results = job.result().get_statevector()
-<<<<<<< HEAD
     del qr
     del qc
     del job
     value = partial_trace(results,[1,2])[0]
     value = np.real(value)
-=======
     value = partial_trace(results,[1,2])
-    print(value)
->>>>>>> 53f6f358e693a66712f6c984395e7787c7fa4546
     return value
-
-<<<<<<< HEAD
-
-
-
-=======
-      
->>>>>>> 53f6f358e693a66712f6c984395e7787c7fa4546
 
 def DSQGOL(nhood):
 
     a = liveliness(nhood)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
+
     value =  nhood[1][1][0]
-=======
-=======
->>>>>>> parent of 2181c44... Update qrules.py
     print(a)
     value =  nhood[1][1]
->>>>>>> parent of 2181c44... Update qrules.py
     alive = [1,0]
     dead = [0,1]
-<<<<<<< HEAD
 
     if value[0] > 0.98:
         if (a < 1 ):
             value = dead
         elif (a > 1 and a <= 1.5):
-=======
-    
+
     if value > 0.98:
         if (a <= 1.5 ):
->>>>>>> 53f6f358e693a66712f6c984395e7787c7fa4546
             value = dead
         elif (a > 1.5 and a <= 2.5):
             print('here')
@@ -161,11 +131,5 @@ def DSQGOL(nhood):
             # job = execute(qci,Aer.get_backend('statevector_simulator'))
             # value = job.result().get_statevector()
         elif (a > 3.5):
-<<<<<<< HEAD
             value=dead
-=======
-            value=dead    
-        print(value) 
-        value = np.array([1.,0.])
->>>>>>> 53f6f358e693a66712f6c984395e7787c7fa4546
     return value
